a0b2b33c5a18da4189af617afb83834c1d67cda9
Ville Manninen <vw.manninen@gmail.com>
2020-08-05 20:22:49 +0300

    added fronent build

---
+++ build/precache-manifest.10ab3eba46452f95486701fd044a9f14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193d9d9dbf0a59df66982e68ca5f4d7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b43850287d1ee1a377a7",
+    "url": "/static/css/main.c02bf5fe.chunk.css"
+  },
+  {
+    "revision": "d72cc7c3f59543947486",
+    "url": "/static/js/2.029bf66b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.029bf66b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43850287d1ee1a377a7",
+    "url": "/static/js/main.4cbaa23b.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
